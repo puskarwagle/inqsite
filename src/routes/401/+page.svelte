87de8442d5f36d38cec624b1ceab81143ea29870
puskarwagl <puskarwagle17@gmail.com>
2025-11-22 02:28:33 +0545

    added pages

---
+++ src/routes/401/+page.svelte
@@ -0,0 +1,153 @@
+
+    <div class="rt-utility-page-wrap rt-color-change">
+      <div class="rt-utility-page-content w-password-page w-form">
+        <form
+          action="/.wf_auth"
+          method="post"
+          id="email-form"
+          name="email-form"
+          data-name="Email Form"
+          class="rt-utility-page-form rt-desktop-text-center w-password-page"
+          data-wf-page-id="67f1853906b43440530f1b2b"
+          data-wf-element-id="67f1853906b43440530f1b3000000000000c"
+        >
+          <div class="rt-password-image-block">
+            <img
+              width="75"
+              height="80"
+              alt="Password Protect"
+              src="https://wubflow-shield.NOCODEXPORT.DEV/67a1ea8462c51e3f81e40a7e/67f2840be64cd5a5f9dfd2e0_Lock.svg"
+              loading="lazy"
+              class="rt-password-image rt-autofit"
+            />
+          </div>
+          <h2 class="rt-gap-off rt-text-color-white">Password protected</h2>
+          <label for="pass" class="rt-password-protect-text w-password-page"
+            >Need support? We’ll reach out to you soon.</label
+          ><input
+            class="rt-password-form-field w-password-page w-input"
+            autofocus="true"
+            maxlength="256"
+            name="pass"
+            data-name="field"
+            placeholder="* Enter your  password"
+            type="password"
+            id="pass"
+          />
+          <div class="w-layout-hflex rt-password-protect-button-wrapper">
+            <div
+              data-w-id="e0c0f3d7-9692-20f3-8ec6-cc84459af244"
+              class="rt-form-button-box rt-position-relative rt-change"
+            >
+              <input
+                type="submit"
+                data-wait="Please wait..."
+                class="rt-form-button w-password-page w-button"
+                value="Submit"
+              />
+              <div class="w-layout-hflex rt-button rt-event-none">
+                <div class="w-layout-hflex rt-button-text-image-wrapper">
+                  <div class="w-layout-hflex rt-button-text-wrap">
+                    <div
+                      data-w-id="f7e48932-40ea-f27b-254f-b2e42f064bff"
+                      class="rt-button-text rt-button-hover-in"
+                    >
+                      submit
+                    </div>
+                    <div
+                      data-w-id="f7e48932-40ea-f27b-254f-b2e42f064c01"
+                      class="rt-button-text rt-button-hover-out"
+                    >
+                      submit
+                    </div>
+                  </div>
+                  <img
+                    width="10"
+                    height="9"
+                    alt="Arrow"
+                    src="https://wubflow-shield.NOCODEXPORT.DEV/67a1ea8462c51e3f81e40a7e/67dcf627abf34dbb0271d792_arrow.svg"
+                    loading="lazy"
+                    class="rt-button-arrow-image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="w-password-page w-form-fail">
+            <div>Incorrect password. Please try again.</div>
+          </div>
+          <div style="display: none" class="w-password-page w-embed w-script">
+            <input
+              type="hidden"
+              name="path"
+              value="<%WF_FORM_VALUE_PATH%>"
+            /><input type="hidden" name="page" value="<%WF_FORM_VALUE_PAGE%>" />
+          </div>
+          <div style="display: none" class="w-password-page w-embed w-script">
+            <script type="application/javascript">
+              (function _handlePasswordPageOnload() {
+                if (/[?&]e=1(&|$)/.test(document.location.search)) {
+                  document.querySelector(
+                    ".w-password-page.w-form-fail",
+                  ).style.display = "block";
+                }
+              })();
+            </script>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="rt-fixed-top">
+      <div class="rt-fixed-tag">
+        <a
+          href="https://www.radianttemplates.com/webflow-templates"
+          target="_blank"
+          class="rt-fixed-icon-1 rt-1 w-inline-block"
+          ><div class="rt-icon-main">
+            <img
+              width="14"
+              height="14"
+              alt=""
+              src="https://wubflow-shield.NOCODEXPORT.DEV/67a1ea8462c51e3f81e40a7e/682eb2409a0169f98c28c620_boxicon.svg"
+              loading="lazy"
+            />
+          </div>
+          <div class="rt-template-div rt-1">
+            <div class="rt-chnage-font">120+ templates</div>
+          </div></a
+        ><a
+          href="https://www.radianttemplates.com/customize-template"
+          target="_blank"
+          class="rt-fixed-icon-1 rt-2 w-inline-block"
+          ><div class="rt-icon-main rt-2">
+            <img
+              width="11"
+              height="11"
+              alt=""
+              src="https://wubflow-shield.NOCODEXPORT.DEV/67a1ea8462c51e3f81e40a7e/682eb2409a0169f98c28c61e_white%20icon.svg"
+              loading="lazy"
+            />
+          </div>
+          <div class="rt-template-div rt-2">
+            <div class="rt-chnage-font rt-change-white">customization</div>
+          </div></a
+        ><a
+          href="https://www.radianttemplates.com/get-quote"
+          target="_blank"
+          class="rt-fixed-icon-1 rt-3 w-inline-block"
+          ><div class="rt-icon-main">
+            <img
+              width="17"
+              height="16"
+              alt=""
+              src="https://wubflow-shield.NOCODEXPORT.DEV/67a1ea8462c51e3f81e40a7e/682eb2409a0169f98c28c61f_box-main.svg"
+              loading="lazy"
+            />
+          </div>
+          <div class="rt-template-div rt3">
+            <div class="rt-chnage-font rt-change-white">Hire Our Team</div>
+          </div></a
+        >
+      </div>
+    </div>
+  
