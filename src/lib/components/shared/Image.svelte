7e9aee78dfe6b0decdf299ff50fb3d2640bc7d12
puskarwagl <puskarwagle17@gmail.com>
2025-11-25 03:27:50 +0545

    404

---
+++ src/lib/components/shared/Image.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let src;
+  export let alt = '';
+  export let width = 'auto';
+  export let height = 'auto';
+  export let loading = 'lazy';
+  export let sizes = '';
+  export let srcset = '';
+  export let className = '';
+
+  import { base } from '$app/paths';
+
+  $: isAbsolute = src && (src.startsWith('http') || src.startsWith('https'));
+</script>
+
+{#if isAbsolute}
+  <img {src} {alt} {width} {height} {loading} {sizes} {srcset} class={className} />
+{:else if src}
+  <img src={base + src} {alt} {width} {height} {loading} {sizes} {srcset} class={className} />
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,24 +2,20 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import Header from '$lib/components/shared/Header.svelte';
+	import { page } from '$app/stores';
 
 	let { children } = $props();
 
-	// Don't show Header on admin page
-	let isAdminPage = $state(false);
-
-	$effect(() => {
-		if (typeof window !== 'undefined') {
-			isAdminPage = window.location.pathname === '/admin';
-		}
-	});
+	// Don't show Header on admin page or error page
+	let isAdminPage = $derived($page.url.pathname === '/admin');
+	let isErrorPage = $derived($page.error);
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{#if !isAdminPage}
+{#if !isAdminPage && !isErrorPage}
 	<Header />
 {/if}
 
