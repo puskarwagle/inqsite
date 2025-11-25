9c13253ec1688724c13deacc305451e960371cb4
puskarwagl <puskarwagle17@gmail.com>
2025-11-26 03:50:20 +0545

    updated vite config with allow list cloudflare url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	server: {
+		allowedHosts: ['bidding-phil-blonde-organized.trycloudflare.com']
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
